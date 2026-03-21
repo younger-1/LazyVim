8bcb6208021bd4e4257de64c9598c6b78688be28
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-03-21 06:40:32 +0100

    fix(tsgo): disable inlayHints.functionLikeReturnTypes by default. Too noisy

--- lua/lazyvim/plugins/extras/lang/typescript/tsgo.lua
+++ lua/lazyvim/plugins/extras/lang/typescript/tsgo.lua
@@ -41,7 +41,7 @@ return {
                 parameterTypes = { enabled = true },
                 variableTypes = { enabled = true },
                 propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = false },
                 enumMemberValues = { enabled = true },
               },
             },
